c93ffffed9627632bd80035854d60fde5fee1c1d
ExoDev <maxim55miui11@gmail.com>
2021-10-21 23:21:46 +0300

    olives: Replace HALs automatically on build

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+# Replace HALs
+# NOTE: It will broke your manifest sync
+
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/display
+rm -rf hardware/qcom-caf/msm8996/media
+
+cd hardware/qcom-caf/msm8996
+
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 audio
+git clone https://github.com/EXOdev-698/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 display
